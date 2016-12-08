04073c50029256229df985d352693e5d78cb4653
luunguyen <luunguyen@ccintegration.com>
2016-12-08 15:53:10 +0700

    UpdAT

--- examples/django/demoapp/tasks.py
+++ examples/django/demoapp/tasks.py
@@ -1,8 +1,18 @@
-from __future__ import absolute_import
-
+# Create your tasks here
+from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
 
 @shared_task
-def test(param):
-    return 'The test task executed with argument "%s" ' % param
+def add(x, y):
+    return x + y
+
+
+@shared_task
+def mul(x, y):
+    return x * y
+
+
+@shared_task
+def xsum(numbers):
+    return sum(numbers)
